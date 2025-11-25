b0d7543659a2317c9893ef3f2028dbec5b90d0b8
ibtihelbs <ibtihel.bensalah@outlook.fr>
2025-11-25 09:44:20 +0100

    slider-fix

--- js/script.js
+++ js/script.js
@@ -20,3 +20,60 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     }
   });
 });
+
+const sliderWrapper = document.querySelector(".mobile-slider");
+
+const cards = document.querySelectorAll(".testimonial-card");
+
+let currentIndex = 0;
+let visibleCards = 3;
+
+function updateVisibleCards() {
+  if (window.innerWidth <= 700) visibleCards = 1;
+  else visibleCards = 3;
+}
+
+function updateSlider() {
+  const offset = -(currentIndex * (100 / visibleCards));
+  sliderWrapper.style.transform = `translateX(${offset}%)`;
+
+  const activeDotIndex = Math.floor(currentIndex / visibleCards);
+  document.querySelectorAll(".dot").forEach((dot, idx) => {
+    dot.classList.toggle("active", idx === activeDotIndex);
+  });
+}
+
+function goToSlide(dotIndex) {
+  currentIndex = dotIndex * visibleCards;
+  updateSlider();
+}
+
+let autoSlideInterval;
+
+function startAutoSlide() {
+  autoSlideInterval = setInterval(() => {
+    const maxCards = cards.length - visibleCards;
+    if (currentIndex < maxCards) {
+      currentIndex += visibleCards;
+    } else {
+      currentIndex = 0;
+    }
+    updateSlider();
+  }, 4000);
+}
+
+function resetAutoSlide() {
+  clearInterval(autoSlideInterval);
+  startAutoSlide();
+}
+
+window.addEventListener("resize", () => {
+  updateVisibleCards();
+  createDots();
+  updateSlider();
+});
+
+updateVisibleCards();
+
+updateSlider();
+startAutoSlide();
